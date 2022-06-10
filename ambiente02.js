9d58642fe949eb1f2e52666a80f7621666571819
EliabGranados <104482173+SamuelGranados@users.noreply.github.com>
2022-06-10 13:00:02 -0300

    vetores

---
+++ ambiente02.js
@@ -0,0 +1,8 @@
+var num = ['5', '0', '9']
+num.push('6', '7')
+num.sort()
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posiçoes`)
+console.log(`o primeiro valor do vetor é ${num[0]}`)
+
+
---
+++ ambiente03.js
@@ -0,0 +1,10 @@
+let valores = ['8', '5', '6', '4', '3', '1', '2', '9']
+valores.sort()
+for(let pos in valores) {
+    console.log(`a posiçao ${pos} tem o valor ${valores[pos]}`)
+}
+/*
+for (let post = 0; post < valores.length; post++) {
+    console.log(`A posiçao ${post} tem o valor ${valores[post]}`)
+} 
+*/
